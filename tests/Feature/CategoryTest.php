bdb631af86fc859e15c08840e2b69c9462792dab
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-20 19:15:16 +0000

    adding tests for update and delete category

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -8,9 +8,8 @@ use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use RefreshDatabase;
+
     public function test_category_creation(): void
     {
         $response = $this->postJson('api/category', ['name' => 'testCategoryOne']);
@@ -19,13 +18,15 @@ class CategoryTest extends TestCase
 
     public function test_category_creation_duplicated_name(): void
     {
+        $this->postJson('api/category', ['name' => 'testCategoryOne']);
         $response = $this->postJson('api/category', ['name' => 'testCategoryOne']);
         $response->assertStatus(422);
     }
 
     public function test_category_creation_with_parent(): void
     {
-        $response = $this->postJson('api/category', ['name' => 'newCategory', 'parent_id' => 1]);
+        $parent = $this->postJson('api/category', ['name' => 'newCategory']);
+        $response = $this->postJson('api/category', ['name' => 'withParent', 'parent_id' => $parent->json()['data']['id']]);
         $response->assertStatus(201);
     }
 
@@ -33,5 +34,19 @@ class CategoryTest extends TestCase
     {
         $response = $this->postJson('api/category', ['name' => 'newCategory', 'parent_id' => 100000000]);
         $response->assertStatus(422);
+    }
+
+    public function test_category_update(): void
+    {
+        $category = $this->postJson('api/category', ['name' => 'testCategoryOne']);
+        $result = $this->putJson('/api/category/'. $category->json()['data']['id'], ['name' => 'testCategoryUpdated']);
+        $result->assertStatus(200);
+    }
+
+    public function test_category_delete(): void
+    {
+        $category = $this->postJson('api/category', ['name' => 'testCategoryOne']);
+        $result = $this->delete('/api/category/'. $category->json()['data']['id']);
+        $result->assertStatus(200);
     }
 }
