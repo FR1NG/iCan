b0fa383b531d2664b3494dc6622d3c5145c2d670
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-21 16:30:57 +0000

    adding a form control adapter component

---
+++ resources/js/components/custom/FormControl.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { watch, ref } from 'vue';
+
+const props = defineProps({
+    width: {
+        default: 'md'
+    },
+    label: '',
+    errorMessage: ''
+});
+
+const hasError = ref(props.errorMessage && props.errorMessage.length > 0 ? true : false);
+
+watch(() => props.errorMessage, (newValue) => {
+    if (newValue.length > 0) {
+        hasError.value = true;
+    }
+    else if(newValue.length === 0 && hasError.value) {
+        hasError.value = false;
+    }
+});
+
+</script>
+
+<template>
+    <div :class="`form-control w-${width}`">
+        <label class="label">
+            <span class="label-text">{{ label }}</span>
+        </label>
+        <slot></slot>
+        <label class="label">
+            <span v-if="hasError" class="label-text-alt text-error">{{errorMessage}}</span>
+        </label>
+    </div>
+</template>
