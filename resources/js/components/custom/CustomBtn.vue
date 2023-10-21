8fb32346256a2330244e5e7e386caa44928c5e3f
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-21 20:02:33 +0000

    crating a custom btn

---
+++ resources/js/components/custom/CustomBtn.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const emit = defineEmits(['click'])
+    const props = defineProps({
+        color: {
+            default: 'secodary'
+        },
+        disabled: {
+            default: false
+        },
+        loading: {
+            default: false
+        }
+
+    });
+    const handleClick = () => {
+        emit('click');
+    }
+    console.log(props.disabled)
+
+</script>
+
+<template>
+    <button :class="`btn ml-2 btn-${color} ${disabled ? 'btn-disabled' : ''}` " @click="handleClick">
+        <slot></slot>
+        <span v-if="loading" class="loading loading-spinner"></span>
+    </button>
+</template>
