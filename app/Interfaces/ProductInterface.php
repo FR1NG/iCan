dd14985f06234803537beff844bb0370fd7f98a4
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-21 13:05:29 +0000

    moving update and delete methods to product interface

--- app/Interfaces/ProductInterface.php
+++ app/Interfaces/ProductInterface.php
@@ -2,6 +2,10 @@
 
 namespace App\Interfaces;
 
+use App\Models\Product;
+
 interface ProductInterface extends BaseInterface
 {
+    public function update(Product $product, array $data);
+    public function delete(Product $product);
 }
