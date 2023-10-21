5b620e9d5de0dcfdde585ce3a23fd870746db9e7
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-21 13:07:52 +0000

    crud of product including image upload

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Interfaces\ProductInterface;
-use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -11,8 +12,43 @@ class ProductController extends Controller
     {
     }
 
+    public function create(ProductRequest $request)
+    {
+        $filename = $this->uploadImage($request);
+        $data = $request->all();
+        $data['image'] = $filename;
+        return $this->product->create($data);
+    }
+
     public function index()
     {
         return $this->product->getAll();
+    }
+
+    public function update(Product $product, ProductRequest $request)
+    {
+        // image update to be handled lather
+        $data = $request->all();
+        // this line to prevent updating image name on database
+        if (isset($data['image'])) {
+            unset($data['image']);
+        }
+        return $this->product->update($product, $data);
+    }
+
+    public function delete(Product $product)
+    {
+        return $this->product->delete($product);
+    }
+
+    private function uploadImage(ProductRequest $request): string
+    {
+        if($request->file('image')){
+            $file= $request->file('image');
+            $filename= date('YmdHi').$file->getClientOriginalName();
+            $file-> move(public_path('Images/Products'), $filename);
+            return $filename;
+        }
+        return 'default.png';
     }
 }
