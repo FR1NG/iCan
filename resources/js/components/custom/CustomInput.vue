78b71e92549e4ef3a7d01bff5c3bed4739ed76f1
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-21 16:31:18 +0000

    adding custom input and custom select

---
+++ resources/js/components/custom/CustomInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import FormControl from './FormControl.vue';
+
+const props = defineProps({
+    modelValue: '',
+    width: {
+        default: 'md'
+    },
+    color: {
+        default: 'primary'
+    },
+    label: '',
+    placeholder: '',
+    errorMessage: ''
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+</script>
+
+<template>
+    <form-control :error-message="errorMessage" :width="width" :label="label">
+        <input
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            type="text"
+            :placeholder="placeholder"
+            :class="`input input-bordered w-full input-${color}`"
+        />
+    </form-control>
+</template>
---
+++ resources/js/components/custom/CustomSelect.vue
@@ -0,0 +1,31 @@
+<script setup>
+import FormControl from './FormControl.vue';
+
+const props = defineProps({
+    modelValue: '',
+    width: {
+        default: 'md'
+    },
+    color: {
+        default: 'primary'
+    },
+    label: '',
+    placeholder: '',
+    errorMessage: '',
+    items: {
+        default: []
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+</script>
+
+<template>
+    <form-control :error-message="errorMessage" :width="width" :label="label">
+        <select class="select select-bordered">
+            <option disabled selected>{{ placeholder }}</option>
+            <option v-for="item in items" :value="item.value">{{ item.text }}</option>
+        </select>
+    </form-control>
+</template>
