81552a39d6923d9f67e24510df1824d73ce743ac
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 17:13:47 +0000

    using daisyui navbar

--- resources/js/components/DefaultLayout.vue
+++ resources/js/components/DefaultLayout.vue
@@ -9,25 +9,34 @@ const routes = [
 
 <template>
 <div class="bg-white">
-  <header class="absolute inset-x-0 top-0 z-50">
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1">
-      </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span class="sr-only">Open main menu</span>
-          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-      </div>
-      <div class="hidden lg:flex lg:gap-x-12">
+<div class="navbar bg-info text-white">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+        <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <router-link v-for="route in routes" :key="route.name" :to="{name: route.name}">
+                <li>
+                    <a>{{ route.text}}</a>
+                </li>
+            </router-link>
+          </ul>
+    </div>
+    <router-link :to="{name: 'Home'}">
+        <a class="btn btn-ghost normal-case text-xl">iCan</a>
+    </router-link>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
         <router-link v-for="route in routes" :key="route.name" :to="{name: route.name}">
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">{{ route.text}}</a>
+            <li>
+                <a>{{ route.text}}</a>
+            </li>
         </router-link>
-      </div>
-    </nav>
-  </header>
+    </ul>
+  </div>
+</div>
 
   <div class="relative isolate px-6 pt-14 lg:px-8">
     <Notification/>
