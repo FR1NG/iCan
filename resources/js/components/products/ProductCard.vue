726f9a60ba6a1e10750136f17466994678af366c
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 11:58:08 +0000

    making a moke product list

---
+++ resources/js/components/products/ProductCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['name', 'description', 'categories', 'image', 'price'])
+
+</script>
+
+<template>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure><img :src="image" alt="Product" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">
+            {{ name }}
+                <div class="badge badge-secondary">{{price}} Dhs</div>
+        </h2>
+            <p>{{ description}}</p>
+        <div class="card-actions justify-end">
+            <div v-for="category in categories" class="badge badge-outline">{{category.name}}</div>
+        </div>
+      </div>
+    </div>
+</template>
--- resources/js/components/products/ProductsList.vue
+++ resources/js/components/products/ProductsList.vue
@@ -1,5 +1,10 @@
+<script setup>
+   import ProductCard from './ProductCard.vue';
+   import CreateProduct from './CreateProduct.vue'
+</script>
 <template>
-    <div>
-      this is products list component
+    <CreateProduct/>
+    <div class="grid  xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  gap-y-8">
+        <ProductCard v-for="n in 5" name="hello" description="this is product" :categories="[{name: 'one'}, {name: 'two'}]" image='/Images/Products/default.png' :price='n'/>
     </div>
 </template>
