95857500d0d854627610e3289249494900ea5a09
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 11:56:12 +0000

    creating a custom input component

---
+++ resources/js/components/custom/CustomFileInput.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FormControl from './FormControl.vue';
+const props = defineProps({
+    modelValue: '',
+    width: {
+        default: 'md'
+    },
+    color: {
+        default: 'primary'
+    },
+    label: '',
+    placeholder: '',
+    errorMessage: ''
+});
+
+const emit = defineEmits(['update:modelValue', 'input']);
+
+const handleInput = (e) => {
+    emit('input', e)
+}
+
+</script>
+
+<template>
+    <form-control :error-message="errorMessage" :width="width" :label="label">
+        <input
+            type="file"
+            :class="`file-input file-input-bordered file-input-${color}`"
+            :value="modelValue"
+            @input="handleInput"
+            :placeholder="placeholder"
+        />
+    </form-control>
+</template>
