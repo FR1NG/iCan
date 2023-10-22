a9222afc1cb29ffe91ce8b12c1cc7c0fb5287f18
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 12:54:54 +0000

    passing categories as prop to create product component

--- resources/js/components/products/ProductsList.vue
+++ resources/js/components/products/ProductsList.vue
@@ -1,9 +1,17 @@
 <script setup>
    import ProductCard from './ProductCard.vue';
    import CreateProduct from './CreateProduct.vue'
+   import { useCategoryStore } from '@/stores/category';
+   import { storeToRefs } from 'pinia';
+
+   const categoryStore = useCategoryStore();
+   const { itemsForSelect } = storeToRefs(categoryStore);
+
+   // getting categories;
+   categoryStore.getCategories()
 </script>
 <template>
-    <CreateProduct/>
+    <CreateProduct :categories="itemsForSelect"/>
     <div class="grid  xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  gap-y-8">
         <ProductCard v-for="n in 5" name="hello" description="this is product" :categories="[{name: 'one'}, {name: 'two'}]" image='/Images/Products/default.png' :price='n'/>
     </div>
