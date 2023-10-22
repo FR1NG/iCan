705358c4696d9e2731f151194e02d0ec1123edee
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 12:55:17 +0000

    creating product component and store

--- resources/js/stores/product.js
+++ resources/js/stores/product.js
@@ -1,15 +1,38 @@
 import { defineStore } from 'pinia';
+import axios from '@/plugins/axios';
 
 export const useProductStore = defineStore('product', {
     state: () => ({
-
+        products: [],
     }),
     getters: {
 
     },
     actions: {
-        test() {
-            console.log('hello from pinia');
+        async getProducts() {
+            return new Promise(async (resolve) => {
+                try {
+                    const response = await axios.get('/product');
+                    this.products = response.data.data;
+                    console.log(this.products)
+                    resolve(response.data.data);
+                } catch(error) {
+                    console.log(error);
+                }
+            })
+        },
+        async createProduct(form) {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    const response = await axios.post('/product', form);
+                    console.log(response.data);
+                    resolve(response.data.data);
+                    this.getProducts();
+                } catch(error) {
+                    console.log(error);
+                    reject(error?.response?.data);
+                }
+            })
         }
     }
 })
