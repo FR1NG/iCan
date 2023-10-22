0cf8f7d0821e5bd27bb8fb940e0fc5110878dc21
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-22 11:57:47 +0000

    removing phone nav from default layout

--- resources/js/components/DefaultLayout.vue
+++ resources/js/components/DefaultLayout.vue
@@ -28,33 +28,6 @@ const routes = [
         </router-link>
       </div>
     </nav>
-    <!-- Mobile menu, show/hide based on menu open state. -->
-    <div class="lg:hidden" role="dialog" aria-modal="true">
-      <!-- Background backdrop, show/hide based on slide-over state. -->
-      <div class="fixed inset-0 z-50"></div>
-      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div class="flex items-center justify-between">
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-            <span class="sr-only">Close menu</span>
-            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-                <router-link :to="{name: 'CreateProduct'}">
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Create Product</a>
-                </router-link>
-                <router-link :to="{name: 'ProductsList'}">
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Products List</a>
-                </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </header>
 
   <div class="relative isolate px-6 pt-14 lg:px-8">
